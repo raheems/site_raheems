6bee9ff9a0374c9513c9e59a5f11a0100d18e06c
Enayet Raheem <raheem@gmail.com>
2018-08-02 16:43:57 -0400

    sandbox.R code added

---
+++ content/R/sandbox.R
@@ -0,0 +1,72 @@
+
+df <- data.frame(
+  group = c("p", "q"),
+  value = c(5, 95)
+)
+
+df
+
+library(ggplot2)
+# Barplot
+bp<- ggplot(df, aes(x="", y=value, fill=group))+
+  geom_bar(width = 1, stat = "identity")
+bp
+
+pie<- bp + coord_polar("y", start=0)
+pie
+
+
+library(sunburstR)
+library(dplyr)
+
+df %>%
+  sunburst(., count = TRUE, colors = c("green", "red"), percent = T, legend = T)
+
+df <- data.frame(
+  x= rnorm(100),
+  y= rnorm(100) + rnorm(100)
+)
+
+head(df)
+
+
+# 2018-08-02 13:18:18 ------------------------------
+
+library(dplyr)
+
+set.seed(100)
+
+df2 <- data.frame(
+  site = rep(c("A", "B", "C", "D"), 20),
+  value = c(rnorm(20, 0, 10), rnorm(20, 9, 5), rnorm(20, 1, 10) , rnorm(20, 5, 10))
+)
+head(df2)
+
+
+df2 = dataset
+
+library(dplyr)
+fit <- aov(value ~ site, data = df2)
+summary(fit)
+a <- unlist(summary(fit))
+p <- as.vector(a[9])
+q <- 1- p
+
+df_p <- data.frame(
+  group = c("p-value", "Acceptance"),
+  value = c(p, q)
+)
+
+df_p
+
+# Pie Chart with Percentages
+slices <- df_p$value
+lbls <- df_p$group
+pct <- round(slices/sum(slices)*100)
+lbls <- paste(lbls, pct) # add percents to labels 
+lbls <- paste(lbls,"%",sep="") # ad % to labels 
+par(oma=c(0,0,0,0))
+pie(slices,labels = lbls, col=c("red", "green"),
+    main="Significant difference, if p < 5%", cex = 1.2)
+
+
